e1f64e4f6161375232d08be898bfb8f6ce29571d
johnny <jhuang007007@gmail.com>
2023-02-27 15:26:44 -0500

    removed post request handling for fix

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -40,12 +40,12 @@ app.get('/api/persons/:id', (request, response, next) => {
 app.post('/api/persons', (request,response) => {
   const body = request.body
 
-  if (body.name === undefined) {
+  if (body.name) {
     return response.status(400).json({
       error: 'name missing'
     })
   }
-  if (body.number === undefined) {
+  if (body.number) {
     return response.status(400).json({
       error: 'number missing'
     })
