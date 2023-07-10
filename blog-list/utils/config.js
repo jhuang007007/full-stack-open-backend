c306d4f90814517480391ca0372e92695c0904ec
johnny <jhuang007007@gmail.com>
2023-07-10 11:47:31 -0400

    undoing changes made in previous commit

--- blog-list/utils/config.js
+++ blog-list/utils/config.js
@@ -3,11 +3,13 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const PORT = process.env.PORT
+console.log(process.env.PORT)
 
 const MONGODB_URI = process.env.NODE_ENV === 'test' 
   ? process.env.TEST_MONGODB_URI
   : process.env.MONGODB_URI
 
+console.log(MONGODB_URI)
 module.exports = {
   MONGODB_URI,
   PORT
